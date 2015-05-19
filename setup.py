69dc0b9041ac4ee6ecb512cea4c80589984b65a0
Dougal Matthews <dougal@redhat.com>
2015-05-19 19:39:23 +0100

    Initial setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.0'
+
+
+setup(
+    name="mkdocs-bootswatch",
+    version=VERSION,
+    url='http://www.mkdocs.org',
+    license='BSD',
+    description='Bootswatch themes for MkDocs',
+    author='Dougal Matthews',
+    author_email='dougal@dougalmatthews.com.com',
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        'mkdocs.themes': [
+            'amelia = mkdocs_bootswatch.amelia',
+            'cerulean = mkdocs_bootswatch.cerulean',
+            'cosmo = mkdocs_bootswatch.cosmo',
+            'cyborg = mkdocs_bootswatch.cyborg',
+            'flatly = mkdocs_bootswatch.flatly',
+            'journal = mkdocs_bootswatch.journal',
+            'readable = mkdocs_bootswatch.readable',
+            'simplex = mkdocs_bootswatch.simplex',
+            'slate = mkdocs_bootswatch.slate',
+            'spacelab = mkdocs_bootswatch.spacelab',
+            'united = mkdocs_bootswatch.united',
+            'yeti = mkdocs_bootswatch.yeti',
+        ]
+    },
+    zip_safe=False
+)
